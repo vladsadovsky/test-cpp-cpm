842038afe64a6c0564d5258d909e27b8f0686200
VladS <vlads_work@outlook.com>
2024-05-31 15:49:06 -0700

    Modify name of the target architecture for native builds to be x64

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ mkdir -p build/debug
 mkdir -p build/release
 
 # Configure a debug build native
-cmake -S . -B build/debug -D CMAKE_BUILD_TYPE=Debug
+cmake -S . -B build/debug/x64 -D CMAKE_BUILD_TYPE=Debug
 # build the debug binaries native
-cmake --build build/debug
+cmake --build build/debug/x64
 
 # Configure a debug build ARM64
  cmake -S . -B build/debug/arm64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=platforms/linux/aarch64-gnu.toolchain.cmake
@@ -20,9 +20,9 @@ cmake --build build/debug/arm64 --verbose
 
 
 # Configure a release build
-cmake -S . -B build/release -D CMAKE_BUILD_TYPE=Release
+cmake -S . -B build/release/x64 -D CMAKE_BUILD_TYPE=Release
 # Build release binaries
-cmake --build build/release
+cmake --build build/release/x64
 
 # Configure a release build
 cmake -S . -B build/release/arm64 -D CMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=platforms/linux/aarch64-gnu.toolchain.cmake
